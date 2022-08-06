4b4aace08d2a3ea54d23e782215f59bf7ee3e9da
David Meredith <dmeredith.1996@gmail.com>
2022-08-06 11:45:46 -0500

    Attempt production fix

--- server/dist/index.js
+++ server/dist/index.js
@@ -7,10 +7,18 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const player_routes_1 = require("./routes/player.routes");
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 const port = process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+    // set static folder
+    app.use(express_1.default.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path_1.default.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 app.use("/api/player", player_routes_1.playerRouter);
 app.listen(port, () => { });
--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { playerRouter } from "./routes/player.routes";
+import path from "path";
 
 dotenv.config();
 
@@ -9,6 +10,14 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT;
+
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.use("/api/player", playerRouter);
 
